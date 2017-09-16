7ea6c738c0b9f1e8f45dfe8321ae809d2236f971
Alexander Mineev <alexandrmineev@gmail.com>
2017-09-16 10:25:04 +0300

    remove unused components

--- DownloadManager/DownloadManager/ClientApp/app/app.module.shared.ts
+++ DownloadManager/DownloadManager/ClientApp/app/app.module.shared.ts
@@ -7,16 +7,12 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './components/app/app.component';
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
-import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
-import { CounterComponent } from './components/counter/counter.component';
 import { FilesComponent } from './components/files/files.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         NavMenuComponent,
-        CounterComponent,
-        FetchDataComponent,
         FilesComponent,
         HomeComponent
     ],
@@ -27,8 +23,6 @@ import { FilesComponent } from './components/files/files.component';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
-            { path: 'counter', component: CounterComponent },
-            { path: 'fetch-data', component: FetchDataComponent },
             { path: 'files', component: FilesComponent },
             { path: '**', redirectTo: 'home' }
         ])
